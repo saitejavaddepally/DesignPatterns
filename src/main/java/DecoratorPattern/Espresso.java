6f5ba4e074e3ad6a4c0f7deb0cbc24393cbda43a
saitejavaddepally <saitejavaddepally11@gmail.com>
2023-12-11 22:31:48 +0530

    added decorator pattern notes

---
+++ src/main/java/DecoratorPattern/Espresso.java
@@ -0,0 +1,8 @@
+package DecoratorPattern;
+
+public class Espresso extends Beverage{
+    @Override
+    double cost() {
+        return 1.99;
+    }
+}
---
+++ src/main/java/DecoratorPattern/Whip.java
@@ -0,0 +1,14 @@
+package DecoratorPattern;
+
+public class Whip extends AddOnDecorator {
+    Beverage beverage;
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+    public double cost() {
+        return .20 + beverage.cost();
+    }
+}
